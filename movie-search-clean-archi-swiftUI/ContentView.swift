8fe1325a3a552e8308d4a288f37a38b876cbcd4b
Noureddine louafi <noureddinelouafi8@gmail.com>
2025-10-21 10:22:24 +0100

    Initial Commit

---
+++ movie-search-clean-archi-swiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie-search-clean-archi-swiftUI
+//
+//  Created by Noureddine Louafi on 21/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie-search-clean-archi-swiftUI/movie_search_clean_archi_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_search_clean_archi_swiftUIApp.swift
+//  movie-search-clean-archi-swiftUI
+//
+//  Created by Noureddine Louafi on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct movie_search_clean_archi_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
